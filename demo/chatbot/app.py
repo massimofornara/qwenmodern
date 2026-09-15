f4f8fc748ac058f3010dfd5015e9cfa19cc29c23
Massimo <mfornara93@gmail.com>
2026-09-16 00:11:06 +0200

    fix: set custom workspace endpoint for US East region

--- demo/chatbot/app.py
+++ demo/chatbot/app.py
@@ -9,6 +9,7 @@ os.system('pip install dashscope')
 import gradio as gr
 from http import HTTPStatus
 import dashscope
+dashscope.base_http_api_url = os.environ.get("DASHSCOPE_HTTP_BASE_URL", "https://ws-9epwqw7luompy0r5.us-east-1.maas.aliyuncs.com/api/v1")
 from dashscope import Generation
 from dashscope.api_entities.dashscope_response import Role
 from typing import List, Optional, Tuple, Dict
